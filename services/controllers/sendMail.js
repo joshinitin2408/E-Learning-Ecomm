6641133e4ca567badbee6ec98ed223630cc6d32c
Nitin Joshi <joshinitin2408@gmail.com>
2024-06-19 02:07:41 +0530

    nodemailer works on ethereal

--- services/controllers/sendMail.js
+++ services/controllers/sendMail.js
@@ -1,6 +1,31 @@
 // ./controllers/sendMail.js
-const sendMail = async (req, res) => {
-    res.send("inside ./controllers/sendMail.js");
-};
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'candido50@ethereal.email',
+        pass: 'B5cwujta342pfADw22'
+    }
+});
+
+async function sendMail(req, res) {
+    try {
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: '"Maddison Foo Koch " <maddison53@ethereal.email>', // sender address
+            to: "nitinjoshimp95@gmail.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+        console.log("Message sent: %s", info.messageId);
+        res.send("Email sent successfully!");
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Failed to send email");
+    }
+}
 
 module.exports = sendMail;
